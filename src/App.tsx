7a75893512b8189b682219b95975736809dcc8ed
Nick Nance <nance.nick@gmail.com>
2019-02-23 22:55:35 -0800

    header and description components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Description from './Description';
+import Header from './Header';
+import logo from './logo.svg';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header name="Nick" />
+        <Description countBy={3} />
       </div>
     );
   }
